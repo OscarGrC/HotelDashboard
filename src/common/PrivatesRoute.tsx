d7e5c6e2e8faf31ef3233516062367f1a101b4a6
Oscar gracia <113419592+OscarGrC@users.noreply.github.com>
2025-02-27 09:54:08 +0100

    mod PrivateRoute

--- src/common/PrivatesRoute.tsx
+++ src/common/PrivatesRoute.tsx
@@ -2,10 +2,15 @@ import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../login/AuthContext.jsx";
 
-const PrivateRoute = ({ element }) => {
-    const { state } = useContext(AuthContext);
 
-    if (!state.isAuthenticated) {
+const PrivateRoute = ({ element }) => {
+    const authContext = useContext(AuthContext);
+
+    if (!authContext) {
+        throw new Error("AuthContext must be used within an AuthProvider");
+    }
+
+    if (!authContext.state.isAuthenticated) {
         return <Navigate to="/login" />;
     }
 
@@ -13,4 +18,3 @@ const PrivateRoute = ({ element }) => {
 };
 
 export default PrivateRoute;
-
